3beeea1a01a1fddda3a0fdb9a30872a1f82f1465
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-05-06 15:53:23 +0530

    refactor(rs): rust update file

--- src/config/parser.rs
+++ src/config/parser.rs
@@ -60,4 +60,52 @@ use std::{
 pub struct Config {
     pub bindings_ip : String,
     pub debug : bool,
+    pub logging_initialized : bool,
+    pub threads : u8,
+}
+
+/* 
+* Documentation : https://doc.rust-lang.org/std/keyword.impl.html#
+*/
+
+/* 
+* The `impl` keyword is primarily used to define implementations on types. Inherent implementations 
+* are standalone, while trait implementations are used to implement traits for types, or other traits.
+* Functions and consts can both be defined in an implementation. A function defined in an impl block 
+* can be standalone, meaning it would be called like Foo::bar(). If the function takes self, &self, 
+* or &mut self as its first argument, it can also be called using method-call syntax, a familiar feature 
+* to any object oriented programmer, like foo.bar().
+*/
+impl Config{
+    pub fn parse(logging_initialized : bool) -> Result<Self, Box<dyn std::error::Error>>{
+        /* 
+        * pub fn parse(logging_initialized: bool) -> Result<Self, Box<dyn std::error::Error>>: 
+        * This is the function signature for a function named parse. It takes a boolean argument 
+        * logging_initialized and returns a Result. The Ok variant contains an instance of Self 
+        * (which refers to the type this method belongs to, likely a Config struct), and the Err 
+        * variant contains a boxed trait object implementing std::error::Error.
+        */
+        let lua = Lua::new();
+        let globals = lua.globals();
+        /* 
+        * let lua = Lua::new();: Creates a new instance of the Lua interpreter.
+        * let globals = lua.globals();: Retrieves a table representing the global environment in Lua.
+        */
+        lua.load(&fs::read_to_string(file_path(FileType::Config)?)?).exec()?;
+        /* 
+        * fs::read_to_string(file_path(FileType::Config)?): Reads the content of the config file 
+        * into a string.
+        * lua.load(&fs::read_to_string(file_path(FileType::Config)?)?): Loads the Lua code from the 
+        * config file into the Lua interpreter.
+        * .exec()?: Executes the loaded Lua code.
+        */
+        let parsed_threads: u8 = globals.get("threads")?;
+        /* 
+        * Retrieves the value associated with the key "threads" from the Lua global 
+        * environment and tries to parse it as a u8. The ? is used to propagate any errors.
+        */
+        let debug: bool = globals.get("debug")?;
+        let logging: bool = globals.get("logging")?;
+        let adaptive_window: bool = globals.get("adaptive_window")?;    
+    }
 }
