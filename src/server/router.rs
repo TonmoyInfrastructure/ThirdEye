48d6245bbea90d83c03b21f6343bfa65ef3a6c1d
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-05-06 14:29:43 +0530

    perf(dep): add actix deps

---
+++ src/server/router.rs
@@ -0,0 +1,14 @@
+/**
+* Documentation: https://crates.io/crates/actix-web
+**/
+use actix_web ::{
+    get,
+    web,
+    App,
+    HttpServer,
+    Responder,
+    http::header::ContentType,
+    httpRequest,
+    HttpResponse,
+};
+
